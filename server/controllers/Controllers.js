3ca84018c41c3c8aeada22703033d0859cbc5ff4
Christian Jaena <christian.jaena@msugensan.edu.ph>
2021-06-08 12:28:44 +0800

    first commit

---
+++ server/controllers/Controllers.js
@@ -0,0 +1,23 @@
+const SampleSchema = require('../models/Model');
+
+function getController(req, res) { }
+
+function getByIdController(req, res) { }
+ 
+function postController(req, res) { }
+
+function updateController(req, res) { }
+
+function deleteController(req, res) { }
+
+function deleteByIdController(req, res) {}
+
+
+module.exports = {
+	getController,
+	getByIdController,
+	postController,
+	updateController,
+	deleteController,
+	deleteByIdController,
+};
