fce6e48d7875bd097206aaab10725437296bb091
Christian Jaena <christian.jaena@msugensan.edu.ph>
2021-06-08 13:44:06 +0800

    added cors dependency

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
